d33f191c412fa443f50e35c0e757ae81d5d8e18f
airbendor890 <airbendor890@gmail.com>
2020-12-12 12:42:17 +0530

    Update q2_main.c

--- q2_main.c
+++ q2_main.c
@@ -21,16 +21,16 @@ int main(){
 
 	double h2=0.2;
 	file1=fopen("q2_fileh2.txt","w");
-	RK4(&f,x_0,y_0,y1_0,x_n,h2,file1);
+	RK4(&f,x_0,y_0,y1_0,x_n,h2,file1.'r');
 	fclose(file1);
 
 	double h3=0.08;
 	file1=fopen("q2_fileh3.txt","w");
-	RK4(&f,x_0,y_0,y1_0,x_n,h3,file1);
+	RK4(&f,x_0,y_0,y1_0,x_n,h3,file1,'r');
 	fclose(file1);
 
 	double h4=0.01;
 	file1=fopen("q2_fileh4.txt","w");
-	RK4(&f,x_0,y_0,y1_0,x_n,h4,file1);
+	RK4(&f,x_0,y_0,y1_0,x_n,h4,file1,'r');
 	fclose(file1);
 }
